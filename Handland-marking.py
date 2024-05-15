40d0a7f1a51799c74f9476c6bdfd5b854f6d1486
Sid <83931879+SidElias@users.noreply.github.com>
2024-05-15 11:33:32 +0900

    Update Handland-marking.py

--- Handland-marking.py
+++ Handland-marking.py
@@ -8,11 +8,11 @@ cap=cv2.VideoCapture(0)
 hands=mphands.Hands()
 while True:
     data,image=cap.read()
-    image in cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+    image in cv2.cvtColor(cv2.flip(image,1))
     results=hands.process(image)
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmakrs(
+            mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks,mphands.HAND_CONNECTIONS)
     cv2.imshow('Handtracker',image)
